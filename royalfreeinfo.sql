0c67a6041de6241b76418af09c7b21d92380eb07
Jay Mehta <jaymehta50@users.noreply.github.com>
2021-02-06 16:40:33 +0000

    Add Create Table command

--- royalfreeinfo.sql
+++ royalfreeinfo.sql
@@ -22,6 +22,10 @@ SET time_zone = "+00:00";
 -- Database: `royalfreeinfo`
 --
 
+CREATE DATABASE `royalfreeinfo`;
+
+use `royalfreeinfo`;
+
 -- --------------------------------------------------------
 
 --
